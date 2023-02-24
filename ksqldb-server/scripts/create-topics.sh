cbe91993c8f43f7333c3f3d3ddd58ddf6ffb44db
twosom <two_somang@icloud.com>
2023-02-24 23:30:02 +0900

    chap8 complete

---
+++ ksqldb-server/scripts/create-topics.sh
@@ -0,0 +1,11 @@
+echo "Waiting for kafka come online..."
+
+cub kafka-ready -b kafka:9092 1 20
+
+# create the users topic
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic users \
+  --replication-factor 1 \
+  --partitions 4 \
+  --create
