05c4a15c538b63b7a3d42d9091145d2f28b45dcd
twosom <two_somang@icloud.com>
2023-02-26 01:48:25 +0900

    chap9 complete

---
+++ ksqldb-with-connector/files/postgres/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE titles
+(
+    id    SERIAL PRIMARY KEY,
+    title VARCHAR(120)
+);
+
+INSERT INTO titles (title)
+values ('Stranger Things');
+INSERT INTO titles (title)
+values ('Black Mirror');
+
